fde0f887e9bcae0d455824d953f1ebf0f3e1be32
Weslen Lima <weslen.blima@gmail.com>
2021-10-08 16:20:11 -0300

    Cusrso Domindo IDEs Java

---
+++ src/com/dio/Primeiro_Programa.java
@@ -0,0 +1,21 @@
+package com.dio;
+
+import com.dio.model.Model;
+
+public class Primeiro_Programa {
+
+	public static void main(String[] args) {
+		
+		Model model = new Model();
+		
+		System.out.println(model);
+		
+
+	}
+
+}
+
+
+
+
+
